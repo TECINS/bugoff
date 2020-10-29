eab27d9c92895148d4fac0e1b533fba9a9d59a2e
Jose Kabo <kabo.0699@gmail.com>
2020-10-28 18:54:25 -0600

    Estructura y login. Ordenar estilos en archivo scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'loading', component: LoadingComponent
+  },
+  {
+    path: 'signup', component: SignUpComponent
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: 'login'
+  },
+  {
+    path: '', pathMatch: 'full', redirectTo: 'login'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
