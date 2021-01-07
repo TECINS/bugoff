624ec7affe8c3c8efdf2c4c80c5c55ab5ad240aa
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2021-01-07 12:07:11 -0600

    correcciones de reporte de errores

--- src/app/components/pages/tester/error-report/error-report.component.ts
+++ src/app/components/pages/tester/error-report/error-report.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { ErrorReportService } from 'src/app/services/tester-Services/tester-report.service';
@@ -18,33 +18,38 @@ export class ErrorReportComponent implements OnInit {
 
   formReport: FormGroup;
   localsession: any;
+  @ViewChild('anexo', {
+    read: ElementRef
+  }) img: ElementRef;
   date: Date;
-  reporteModel: errorReport = new errorReport();
   constructor(
     private reportErrorService: ErrorReportService,
     public fb: FormBuilder,
-    private router: Router,
     private utilService: UtilService,
     private datePipe: DatePipe
   ) {
     this.formReport = fb.group({
-      titulo: ['', [ Validators.required]],
+      autor_reporte: [''],
+      // TODO: sacar el id_proyectos dependiendo del proyecto actual
+      id_proyectos: ['1851147215'],
+      fecha_reporte: [''],
+      titulo_error: ['', [Validators.required]],
       iteraciones: ['', [Validators.required]],
-      porcentaje: ['', [Validators.required]],
-      dispositivos: ['', [Validators.required]],
+      porcentaje_aparicion: ['', [Validators.required]],
+      dispositivo_uso: ['', [Validators.required]],
       descripcion: ['', [Validators.required]],
-      prioridad: ['', [Validators.required]],
-      anexo: ['', [Validators.required]],
-      ramas: ['', [Validators.required]],
+      id_prioridades: ['', [Validators.required]],
+      anexo: ['anexo', [Validators.required]],
+      rama_repositorio: ['', [Validators.required]],
     });
     this.date = new Date();
     this.localsession = JSON.parse(localStorage.getItem('session-bugoff'));
-   }
+  }
 
   ngOnInit(): void {
   }
   //#region Chips for Test devices
-  visible= true;
+  visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
@@ -81,7 +86,7 @@ export class ErrorReportComponent implements OnInit {
 
     // Add testings
     if ((value || '').trim()) {
-      this.branches.push( value.trim() );
+      this.branches.push(value.trim());
     }
 
     // Reset the input value
@@ -102,51 +107,53 @@ export class ErrorReportComponent implements OnInit {
   imageSrc = 'https://scontent.frex1-1.fna.fbcdn.net/v/t1.0-9/60448348_2188457861222810_257805742654881792_o.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeGzH1r1DSACU8LOsowxysLYql9H1etFXR6qX0fV60VdHvTLXZJvqsAJPT3T7YbHmSXUhGAb7m7Ur0381ijl1_D5&_nc_ohc=nep7fHZJzKIAX_JZbSd&_nc_ht=scontent.frex1-1.fna&oh=6076f6184390b08d23b7eddd7c93c825&oe=5FFF461E';
   readURL(event: Event): void {
     if ((event.target as HTMLInputElement).files && (event.target as HTMLInputElement).files[0]) {
-        const file = (event.target as HTMLInputElement).files[0];
-        const reader = new FileReader();
-        reader.onload = e => this.imageSrc = reader.result.toString();
-        reader.readAsDataURL(file);
+      const file = (event.target as HTMLInputElement).files[0];
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result.toString();
+      reader.readAsDataURL(file);
     }
   }
-  //#endregion
-  getInfo(){
-    this.reporteModel.anexo = this.formReport.get('anexo').value;
-    this.reporteModel.autor_reporte = this.localsession.user_name;
-    this.reporteModel.descricpion = this.formReport.get('descripcion').value;
-    this.reporteModel.dispositivo_uso = this.testings.join();
-    this.reporteModel.fecha_reporte = this.datePipe.transform(this.date, "yyyy-MM-dd");
-    this.reporteModel.id_prioridades = Number(this.formReport.get('prioridad').value);
-    this.reporteModel.id_proyectos = 140;
-    this.reporteModel.iteraciones = Number(this.formReport.get('iteraciones').value);
-    this.reporteModel.porcentaje_aparicion = Number(this.formReport.get('porcentaje').value);
-    this.reporteModel.rama_repositorio = this.branches.join();
-    this.reporteModel.titulo_error = this.formReport.get('titulo').value;
-    return this.reporteModel;
-  }
-  reportarerrorEvent(){
-    this.utilService._loading = true;
-    if( this.formReport.invalid){
-      return Object.values( this.formReport.controls).forEach( control => {
+  reportarerrorEvent(): void {
+    if (this.formReport.invalid) {
+      return Object.values(this.formReport.controls).forEach(control => {
         control.markAsTouched();
         Swal.fire({
           icon: 'warning',
           title: 'Verifica que no haya campos vacios'
         });
       });
+    } else {
+      const anexo = this.img.nativeElement.files[0];
+      const data = new FormData();
+      data.append('anexo', anexo);
+      this.formReport.get('fecha_reporte').setValue(this.datePipe.transform(this.date, 'yyyy-MM-dd'));
+      this.formReport.get('autor_reporte').setValue(this.localsession.id_usuarios);
+      this.formReport.get('anexo').setValue(anexo);
+      this.utilService._loading = true;
+      console.log(this.formReport.value);
+      this.reportErrorService.errorReport(this.formReport.value)
+        .subscribe(
+          (resp) => {
+            console.log(resp);
+            if (!resp.error) {
+              Swal.fire({
+                allowOutsideClick: false,
+                icon: 'info',
+                title: 'Error reportado',
+              });
+              this.formReport.reset();
+            } else {
+              Swal.fire({
+                allowOutsideClick: false,
+                icon: 'info',
+                title: 'Ocurrio un error al reportar',
+              });
+            }
+          },
+          err => {
+            console.log(err);
+          }
+        ).add(() => this.utilService._loading = false);
     }
-    this.reportErrorService.errorReport(this.getInfo())
-      .subscribe(
-        (resp) => {
-          Swal.fire({
-            allowOutsideClick: false,
-            icon: 'info',
-            title: resp['message'],
-          });
-          this.formReport.reset();
-        },
-        err => {
-          console.log(err);
-        }
-      ).add(() => this.utilService._loading = false);
   }
 }
